5d8cf97c32614cdff25707f8040b9e5b3590a403
zegameiro <jose.mcgameiro@ua.pt>
2024-05-09 19:27:34 +0100

    Dockerized frontend projects and fixed tailwindcss configs

--- digital_screen/vite.config.js
+++ digital_screen/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    },
+    hmr: {
+      overlay: true
+    },
+    host: true,
+    strictPort: true,
+    port: 3003,
+  }
 })
--- staff_app/src/App.jsx
+++ staff_app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex flex-row justify-center'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='font-bold'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
