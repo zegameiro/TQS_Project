64ea1c5b9941cad113a2eb337e138fa6564ad7c2
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-05-10 10:44:03 +0100

    TQS-13 translating things to English

--- staff_app/src/routes/AdminPage.jsx
+++ staff_app/src/routes/AdminPage.jsx
@@ -10,11 +10,11 @@ export default function AdminPage() {
         <div className="pt-5">
           <Table hoverable>
             <Table.Head>
-              <Table.HeadCell>Nome</Table.HeadCell>
-              <Table.HeadCell>Posição</Table.HeadCell>
-              <Table.HeadCell>Estado</Table.HeadCell>
+              <Table.HeadCell>Name</Table.HeadCell>
+              <Table.HeadCell>Function</Table.HeadCell>
+              <Table.HeadCell>Status</Table.HeadCell>
               <Table.HeadCell>
-                <span className="sr-only">Editar</span>
+                <span className="sr-only">Edit</span>
               </Table.HeadCell>
             </Table.Head>
             <Table.Body className="divide-y">
@@ -22,14 +22,14 @@ export default function AdminPage() {
                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                   {"Ana Silva"}
                 </Table.Cell>
-                <Table.Cell>Cabeleireira</Table.Cell>
+                <Table.Cell>Hairdresser</Table.Cell>
                 <Table.Cell>Online</Table.Cell>
                 <Table.Cell>
                   <a
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
-                    Editar
+                    Edit
                   </a>
                 </Table.Cell>
               </Table.Row>
@@ -37,14 +37,14 @@ export default function AdminPage() {
                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                   Bruno Duarte
                 </Table.Cell>
-                <Table.Cell>Cabeleireiro</Table.Cell>
+                <Table.Cell>Hairdresser</Table.Cell>
                 <Table.Cell>Online</Table.Cell>
                 <Table.Cell>
                   <a
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
-                    Editar
+                    Edit
                   </a>
                 </Table.Cell>
               </Table.Row>
@@ -52,14 +52,14 @@ export default function AdminPage() {
                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                   Carolina Guerreira
                 </Table.Cell>
-                <Table.Cell>Massagista</Table.Cell>
+                <Table.Cell>Masseuse</Table.Cell>
                 <Table.Cell>Offline</Table.Cell>
                 <Table.Cell>
                   <a
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
-                    Editar
+                    Edit
                   </a>
                 </Table.Cell>
               </Table.Row>
