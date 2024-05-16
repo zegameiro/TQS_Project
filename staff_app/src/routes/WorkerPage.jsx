b6b05d9656f3b808efb05dd6cee16a50ea1ace10
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-05-16 22:39:30 +0100

    TQS-82 small div change and margin adjusted

--- staff_app/src/routes/WorkerPage.jsx
+++ staff_app/src/routes/WorkerPage.jsx
@@ -7,7 +7,7 @@ export default function WorkerPage() {
     <>
       <div>
         <NavbarComponent />
-        <div className="pt-5">
+        <div className="pt-5 mx-10">
           <Popover
             aria-labelledby="default-popover"
             content={
@@ -29,38 +29,42 @@ export default function WorkerPage() {
           >
             <Button>Call next customer!</Button>
           </Popover>
-        </div>
-        <div className="overflow-x-auto pt-5">
-          <Table striped>
-            <Table.Head>
-              <Table.HeadCell>Customer</Table.HeadCell>
-              <Table.HeadCell>Service</Table.HeadCell>
-              <Table.HeadCell>Category</Table.HeadCell>
-              <Table.HeadCell>Timeslot</Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y">
-              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  Manuela Almeida
-                </Table.Cell>
-                <Table.Cell>Highlights</Table.Cell>
-                <Table.Cell>Hairdressing</Table.Cell>
-                <Table.Cell>10h15</Table.Cell>
-              </Table.Row>
-              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Miguel Aguiar</Table.Cell>
-                <Table.Cell>Buzzcut</Table.Cell>
-                <Table.Cell>Hairdressing</Table.Cell>
-                <Table.Cell>10h45</Table.Cell>
-              </Table.Row>
-              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Nuno Ronaldo</Table.Cell>
-                <Table.Cell>Manicure</Table.Cell>
-                <Table.Cell>Manicure/Pedicure</Table.Cell>
-                <Table.Cell>11h00</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+          <div className="overflow-x-auto pt-5">
+            <Table striped>
+              <Table.Head>
+                <Table.HeadCell>Customer</Table.HeadCell>
+                <Table.HeadCell>Service</Table.HeadCell>
+                <Table.HeadCell>Category</Table.HeadCell>
+                <Table.HeadCell>Timeslot</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Manuela Almeida
+                  </Table.Cell>
+                  <Table.Cell>Highlights</Table.Cell>
+                  <Table.Cell>Hairdressing</Table.Cell>
+                  <Table.Cell>10h15</Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Miguel Aguiar
+                  </Table.Cell>
+                  <Table.Cell>Buzzcut</Table.Cell>
+                  <Table.Cell>Hairdressing</Table.Cell>
+                  <Table.Cell>10h45</Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Nuno Ronaldo
+                  </Table.Cell>
+                  <Table.Cell>Manicure</Table.Cell>
+                  <Table.Cell>Manicure/Pedicure</Table.Cell>
+                  <Table.Cell>11h00</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </div>
         </div>
       </div>
     </>
