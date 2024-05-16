f77d004001ad678bba453de352c249caa39e8a6e
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-05-16 18:57:17 +0100

    TQS-80 Using real data on the facilities table

--- staff_app/src/components/AdminFacilityModal.jsx
+++ staff_app/src/components/AdminFacilityModal.jsx
@@ -1,9 +1,8 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { Button, Label, Modal, TextInput } from "flowbite-react"
-// import { useEffect, useState } from "react"
 import PropTypes from "prop-types"
-import { useMutation } from "@tanstack/react-query"
-import { addNewFacility } from "../../actions/postActions"
 import { useForm } from "react-hook-form"
+import { addNewFacility } from "../../actions/postActions"
 import axios from "../../api"
 
 AdminFacilityModal.propTypes = {
@@ -19,47 +18,30 @@ export default function AdminFacilityModal({
   facilityData,
   mode,
 }) {
-  // const [name, setName] = useState("")
-  // const [city, setCity] = useState("")
-  // const [streetName, setStreetName] = useState("")
-  // const [postalCode, setPostalCode] = useState("")
-  // const [phoneNumber, setPhoneNumber] = useState("")
+  const queryClient = useQueryClient()
 
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors }
-  } = useForm();
+    formState: { errors },
+  } = useForm()
 
-  // useEffect(() => {
-  //   if (mode === "edit" && facilityData) {
-  //     setName(facilityData.name || "")
-  //     setCity(facilityData.city || "")
-  //     setStreetName(facilityData.streetName || "")
-  //     setPostalCode(facilityData.postalCode || "")
-  //     setPhoneNumber(facilityData.phoneNumber || "")
-  //   } else {
-  //     setName("")
-  //     setCity("")
-  //     setStreetName("")
-  //     setPostalCode("")
-  //     setPhoneNumber("")
-  //   }
-  // }, [openModal, mode, facilityData])
-
-  function onCloseModal() {
+  const onCloseModal = () => {
     setOpenModal(false)
   }
 
   const addFacilityMutation = useMutation({
     mutationKey: ["addFacility"],
     mutationFn: (facilityData) => addNewFacility(axios, facilityData),
+    onSuccess: () => {
+      queryClient.refetchQueries("allFacilities")
+    },
   })
 
   const onSubmit = (data) => {
-    console.log(data);
     addFacilityMutation.mutate(data)
+    onCloseModal()
   }
 
   return (
@@ -76,86 +58,117 @@ export default function AdminFacilityModal({
                 <Label htmlFor="name" value="Name" />
               </div>
               <TextInput
-                {...register("name", { 
+                {...register("name", {
                   required: "This fields is required",
                   maxLength: { value: 50, message: "The name is too long" },
-                  minLength: { value: 5, message: "The name is too short" }
+                  minLength: { value: 5, message: "The name is too short" },
                 })}
                 id="name"
                 placeholder="The facilities name"
               />
-              {errors.name && <span className="text-red-500">{errors.name?.message}</span>}
+              {errors.name && (
+                <span className="text-red-500">{errors.name?.message}</span>
+              )}
             </div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="city" value="City" />
               </div>
               <TextInput
-                {...register("city", { 
+                {...register("city", {
                   required: "This field is required",
                   maxLength: { value: 40, message: "The city is too long" },
-                  minLength: { value: 5, message: "The city is too short" }
+                  minLength: { value: 5, message: "The city is too short" },
                 })}
                 id="city"
                 placeholder="The facilities city"
               />
-              {errors.city && <span className="text-red-500">{errors.city?.message}</span>}
+              {errors.city && (
+                <span className="text-red-500">{errors.city?.message}</span>
+              )}
             </div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="streetName" value="Street Name" />
               </div>
               <TextInput
-                {...register("streetName", { 
+                {...register("streetName", {
                   required: "This field is required",
-                  maxLength: { value: 70, message: "The street name is too long" },
-                  minLength: { value: 5, message: "The street name is too short" }
+                  maxLength: {
+                    value: 70,
+                    message: "The street name is too long",
+                  },
+                  minLength: {
+                    value: 5,
+                    message: "The street name is too short",
+                  },
                 })}
                 id="streetName"
                 placeholder="The street name where the facility is located"
               />
-              {errors.streetName && <span className="text-red-500">{errors.streetName?.message}</span>}
+              {errors.streetName && (
+                <span className="text-red-500">
+                  {errors.streetName?.message}
+                </span>
+              )}
             </div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="postalCode" value="Postal Code" />
               </div>
               <TextInput
-                {...register("postalCode", { 
+                {...register("postalCode", {
                   required: "This field is required",
-                  maxLength: { value: 8, message: "The postal code is too long" },
-                  minLength: { value: 8, message: "The postal code is too short" }
+                  maxLength: {
+                    value: 8,
+                    message: "The postal code is too long",
+                  },
+                  minLength: {
+                    value: 8,
+                    message: "The postal code is too short",
+                  },
                 })}
                 id="postalCode"
                 placeholder="The postal code of the facility"
               />
-              {errors.postalCode && <span className="text-red-500">{errors.postalCode?.message}</span>}
+              {errors.postalCode && (
+                <span className="text-red-500">
+                  {errors.postalCode?.message}
+                </span>
+              )}
             </div>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="phoneNumber" value="Phone Number" />
               </div>
               <TextInput
-                {...register("phoneNumber", { 
+                {...register("phoneNumber", {
                   required: "This field is required",
-                  pattern: { value: /^\d{9}$/, message: "The phone number must have 9 digits"},
-                  maxLength: { value: 9, message: "The phone number is too long" },
+                  pattern: {
+                    value: /^\d{9}$/,
+                    message: "The phone number must have 9 digits",
+                  },
+                  maxLength: {
+                    value: 9,
+                    message: "The phone number is too long",
+                  },
                 })}
                 id="phoneNumber"
                 placeholder="The phone number of the facility"
               />
-              {errors.phoneNumber && <span className="text-red-500">{errors.phoneNumber?.message}</span>}
+              {errors.phoneNumber && (
+                <span className="text-red-500">
+                  {errors.phoneNumber?.message}
+                </span>
+              )}
             </div>
 
             <div className="flex flex-row mt-4 space-x-5">
               <Button type="submit">
                 {mode === "edit" ? "Save Changes" : "Create Facility"}
               </Button>
-              <Button onClick={() => reset()}>
-                Clear
-              </Button>
+              <Button onClick={() => reset()}>Clear</Button>
             </div>
-
           </form>
         </div>
       </Modal.Body>
