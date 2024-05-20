1d93c359ff38214c6aacd70dcff7c042fadf6849
zegameiro <jose.mcgameiro@ua.pt>
2024-05-20 01:27:41 +0100

    TQS-26 Changed something in Room Entity and repository

--- backend/src/main/java/deti/tqs/backend/repositories/RoomRepository.java
+++ backend/src/main/java/deti/tqs/backend/repositories/RoomRepository.java
@@ -1,9 +1,17 @@
 package deti.tqs.backend.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import deti.tqs.backend.models.Room;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+  Room findById(long id);
+
+  Room findByName(String name);
+
+  Room findByFacilityId(long id);
   
+  Room findByNameAndFacilityId(String name, long facilityId);
 }
