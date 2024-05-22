5530dea8332d83823f2228159a01901dd3c414ac
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-05-23 00:22:12 +0100

    TQS-27 add new actions to frontend

--- staff_app/actions/getActions.jsx
+++ staff_app/actions/getActions.jsx
@@ -1,5 +1,23 @@
 export const getAllFacilities = async (axios) => {
   return axios.get("facility/all").then((res) => {
-    return res.data;
-  });
+    return res.data
+  })
+}
+
+export const getAllRooms = async (axios) => {
+  return axios.get("room/all").then((res) => {
+    return res.data
+  })
+}
+
+export const getRoomById = async (axios, id) => {
+  return axios.get(`room/${id}`).then((res) => {
+    return res.data
+  })
+}
+
+export const getRoomsByFacilityID = async (axios, facilityID) => {
+  return axios.get(`room/search?facilityID=${facilityID}`).then((res) => {
+    return res.data
+  })
 }
