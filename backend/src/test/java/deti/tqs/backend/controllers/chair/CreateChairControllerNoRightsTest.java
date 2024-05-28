9cdf181bff2a3047ae143107ef592f5a658a9af0
Rafael Ferreira <rafaelcferreira@ua.pt>
2024-05-28 01:10:25 +0100

    TQS-29 Fixed wrong file

--- backend/src/test/java/deti/tqs/backend/controllers/chair/CreateChairControllerNoRightsTest.java
+++ backend/src/test/java/deti/tqs/backend/controllers/chair/CreateChairControllerNoRightsTest.java
@@ -17,9 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import deti.tqs.backend.controllers.ChairController;
-import deti.tqs.backend.dtos.ChairSchema;
-import deti.tqs.backend.models.Chair;
-import deti.tqs.backend.models.Room;
 import deti.tqs.backend.services.ChairService;
 
 @WebMvcTest(ChairController.class)
@@ -36,25 +33,17 @@ class CreateChairControllerNoRightsTest {
   @MockBean
   private ChairService chairService;
 
-  private ChairSchema cs1;
-  private Chair c1 = new Chair();
-  private Room r1 = new Room();
-
   /*
    * NECESSARY TESTS
    * 
-   *  1. Create a chair with success
-   *  2. Create a chair with a name that already exists in a room
-   *  3. Create a chair with an invalid name
-   *  4. Create a chair to a room that does not exists
-   *  5. Create a chair in a room that already has the maximum number of chairs filled
+   *  1. Fail to create a chair without admin rights
    * 
   */
 
 
   @Test
   @DisplayName("Create a chair with success")
-  void createChairWithSuccess() throws Exception {
+  void failtToCreateChairWithoutAdminRights() throws Exception {
     mvc.perform(
       post("/api/chair/admin/add")
         .contentType(MediaType.APPLICATION_JSON)
@@ -64,67 +53,6 @@ class CreateChairControllerNoRightsTest {
 
     verify(chairService, times(0)).addChair(any(), anyLong());
 
-  }
-
-  @Test
-  @DisplayName("Create a chair with a name that already exists in a room")
-  void createChairWithExistingName() throws Exception {
-    
-    mvc.perform(
-      post("/api/chair/admin/add")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{\"Something\": \"Anything\"}")
-    )
-    .andExpect(status().isForbidden());
-
-    verify(chairService, times(0)).addChair(any(), anyLong());
-
-  }
-
-  @Test
-  @DisplayName("Create a chair with an invalid name")
-  void createChairWithInvalidName() throws Exception {
-
-    mvc.perform(
-      post("/api/chair/admin/add")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{\"Something\": \"Anything\"}")
-    )
-    .andExpect(status().isForbidden());
-
-    verify(chairService, times(0)).addChair(any(), anyLong());
-
-  }
-
-  @Test
-  @DisplayName("Create a chair to a room that does not exists")
-  void createChairToNonExistingRoom() throws Exception {
-
-    mvc.perform(
-      post("/api/chair/admin/add")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{\"Something\": \"Anything\"}")
-    )
-    .andExpect(status().isForbidden());
-
-    verify(chairService, times(0)).addChair(any(), anyLong());
-
-  }
-
-  @Test
-  @DisplayName("Create a chair in a room that already has the maximum number of chairs filled")
-  void createChairInFullRoom() throws Exception {
-    
-    mvc.perform(
-      post("/api/chair/admin/add")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{\"Something\": \"Anything\"}")
-    )
-    .andExpect(status().isForbidden());
-
-    verify(chairService, times(0)).addChair(any(), anyLong());
-
-  }
-  
+  }  
 
 }
