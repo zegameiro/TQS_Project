f001476a6ebc94ae303f22d5c26769e228bebf35
Rafael Ferreira <rafaelcferreira@ua.pt>
2024-05-29 19:45:10 +0100

    TQS-71 Flesh out Employee Controller

--- backend/src/main/java/deti/tqs/backend/controllers/EmployeeController.java
+++ backend/src/main/java/deti/tqs/backend/controllers/EmployeeController.java
@@ -2,15 +2,18 @@ package deti.tqs.backend.controllers;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.status.Status;
 import deti.tqs.backend.dtos.EmployeeSchema;
 import deti.tqs.backend.models.Employee;
 import deti.tqs.backend.services.EmployeeService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.persistence.EntityExistsException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,10 +32,35 @@ public class EmployeeController {
     }
 
     @PostMapping("/admin/add")
-    public ResponseEntity<Employee> createEmployee(@RequestBody(required = true) EmployeeSchema employeeSchema)
-            throws Exception {
-        // TOTO: Implement this method
-        throw new UnsupportedOperationException("Not implemented yet");
+    public ResponseEntity<Employee> createEmployee(@RequestBody(required = true) EmployeeSchema employeeSchema) {
+
+        
+
+        logger.info("Creating employee");
+
+        Employee employee;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        try {
+            employee = new Employee();
+            employee.setAdmin(employeeSchema.isAdmin());
+            employee.setFullName(employeeSchema.fullName());
+            employee.setEmail(employeeSchema.email());
+            employee.setPhoneNumber(employeeSchema.phoneNumber());
+            employee.setSpecialitiesID(employeeSchema.specialitiesID());
+
+
+            employee = employeeService.save(employee);
+            
+            status = HttpStatus.CREATED;
+            return new ResponseEntity<Employee>(employee, status);
+        } catch (EntityExistsException e) {
+            status = HttpStatus.CONFLICT;
+        } catch (IllegalArgumentException e) {
+            status = HttpStatus.BAD_REQUEST;
+        }
+
+        return new ResponseEntity<Employee>(status);
     }
 
 }
