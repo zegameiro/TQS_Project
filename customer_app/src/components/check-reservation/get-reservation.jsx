743acee452db6753ab96751356c46c74aa6e6891
GuiAmorim03 <guilhermeamorim@ua.pt>
2024-05-30 16:42:43 +0100

    TQS-74 not found reservation

--- customer_app/src/components/check-reservation/get-reservation.jsx
+++ customer_app/src/components/check-reservation/get-reservation.jsx
@@ -26,19 +26,21 @@ const GetReservation = ({ token, setCurrentStep }) => {
         queryKey: ["reservation", token],
         queryFn: () => getReservationBySecretCode(axios, token),
     });
-    // console.log(reservationGet?.data)
     const reservation = {}
-    reservation.costumer = 
-    {
-        "name" : reservationGet.data?.customerName,
-        "email" : reservationGet.data?.customerEmail,
-        "phone" : reservationGet.data?.customerPhoneNumber
-    }
 
-    reservation.reservationDetails =
-    {
-        "employee" : reservationGet.data?.employee.fullName,
-        "date" : new Date(reservationGet.data?.timestamp).toLocaleString()
+    if (reservationGet?.data !== '') {
+        reservation.costumer =
+        {
+            "name": reservationGet.data?.customerName,
+            "email": reservationGet.data?.customerEmail,
+            "phone": reservationGet.data?.customerPhoneNumber
+        }
+
+        reservation.reservationDetails =
+        {
+            "employee": reservationGet.data?.employee.fullName,
+            "date": new Date(reservationGet.data?.timestamp).toLocaleString()
+        }
     }
 
     // Modal
@@ -52,35 +54,40 @@ const GetReservation = ({ token, setCurrentStep }) => {
         <>
             {reservationGet &&
                 <>
-                    <div className="h-[40vh]" style={{ display: 'flex', flexDirection: 'row', overflowY: "auto" }}>
+                    {reservationGet.data !== '' ? (
+                        <>
+                            <div className="h-[40vh]" style={{ display: 'flex', flexDirection: 'row', overflowY: "auto" }}>
 
-                        <div className="w-[45vw] px-5">
-                            <h1 className="text-3xl font-bold mb-5" >Costumer Details</h1>
-                            <ReservationField label="Name" value={reservation.costumer.name} />
-                            <ReservationField label="Email" value={reservation.costumer.email} />
-                            <ReservationField label="Phone" value={reservation.costumer.phone} />
+                                <div className="w-[45vw] px-5">
+                                    <h1 className="text-3xl font-bold mb-5" >Costumer Details</h1>
+                                    <ReservationField label="Name" value={reservation.costumer.name} />
+                                    <ReservationField label="Email" value={reservation.costumer.email} />
+                                    <ReservationField label="Phone" value={reservation.costumer.phone} />
+                                </div>
+
+                                <div className="w-[45vw] px-5">
+                                    <h1 className="text-3xl font-bold mb-5" >Reservation Details</h1>
+                                    {/* <ReservationField label="Facility" value={reservation.reservationDetails.facility} /> */}
+                                    {/* <ReservationField label="Section" value={reservation.reservationDetails.section} /> */}
+                                    {/* <ReservationField label="Price" value={reservation.reservationDetails.price} /> */}
+                                    <ReservationField label="Employee" value={reservation.reservationDetails.employee} />
+                                    <ReservationField label="Date" value={reservation.reservationDetails.date} />
+                                </div>
+
+                            </div>
+
+                            <div className="mt-5" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <Button color="primary" className="text-white m-1 mt-8" size="lg" onClick={() => setCurrentStep(0)}>Search Another Reservation</Button>
+                                <Button color="danger" className="text-white m-1 mt-8" size="lg" onPress={onOpen}>Cancel Reservation</Button>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="flex justify-center items-center h-[100%]" style={{ flexDirection: 'column' }}>
+                            <h1 className="text-5xl text-danger font-bold">Reservation not found</h1>
+                            <Button color="primary" className="text-white mt-[20vh]" size="lg" onClick={() => setCurrentStep(0)}>Back</Button>
                         </div>
-
-                        <div className="w-[45vw] px-5">
-                            <h1 className="text-3xl font-bold mb-5" >Reservation Details</h1>
-                            {/* <ReservationField label="Facility" value={reservation.reservationDetails.facility} /> */}
-                            {/* <ReservationField label="Section" value={reservation.reservationDetails.section} /> */}
-                            {/* <ReservationField label="Price" value={reservation.reservationDetails.price} /> */}
-                            <ReservationField label="Employee" value={reservation.reservationDetails.employee} />
-                            <ReservationField label="Date" value={reservation.reservationDetails.date} />
-                        </div>
-
-                    </div>
-
-                    <div className="mt-5" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                        <Button color="primary" className="text-white m-1 mt-8" size="lg" onClick={() => setCurrentStep(0)}>Search Another Reservation</Button>
-                        <Button color="danger" className="text-white m-1 mt-8" size="lg" onPress={onOpen}>Cancel Reservation</Button>
-                    </div>
+                    )}
                 </>
-                // <div className="flex justify-center items-center h-[100%]" style={{ flexDirection: 'column' }}>
-                //     <h1 className="text-5xl text-danger font-bold">Reservation not found</h1>
-                //     <Button color="primary" className="text-white mt-[20vh]" size="lg" onClick={() => setCurrentStep(0)}>Back</Button>
-                // </div>
             }
 
             <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
